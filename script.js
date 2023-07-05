b9395d695c9b7e71c83649334ff4f6d2d942947b
Faber David Sanchez Martínez <121761167+Dafer24@users.noreply.github.com>
2023-07-05 15:37:07 -0500

    Avances16

--- script.js
+++ script.js
@@ -7,9 +7,13 @@ function encryptText() {
 
 function decryptText() {
     var text = document.getElementById("external-text").value.toLowerCase();
-    var result = decrypt(text);
     
-    document.getElementById("internal-text").value = result;
+    if (isEncrypted(text)) {
+        var result = decrypt(text);
+        document.getElementById("internal-text").value = result;
+    } else {
+        alert("El texto no está encriptado. No se puede desencriptar.");
+    }
 }
 
 function encrypt(text) {
@@ -23,13 +27,27 @@ function encrypt(text) {
 }
 
 function decrypt(text) {
-    text = text.replace(/enter/g, "e");
-    text = text.replace(/imes/g, "i");
-    text = text.replace(/ai/g, "a");
-    text = text.replace(/ober/g, "o");
-    text = text.replace(/ufat/g, "u");
-    
-    return text;
+    if (isEncrypted(text)) {
+        text = text.replace(/enter/g, "e");
+        text = text.replace(/imes/g, "i");
+        text = text.replace(/ai/g, "a");
+        text = text.replace(/ober/g, "o");
+        text = text.replace(/ufat/g, "u");
+
+        return text;
+    } else {
+        return text;
+    }
+}
+
+function isEncrypted(text) {
+    var keywords = ["enter", "imes", "ai", "ober", "ufat"];
+    for (var i = 0; i < keywords.length; i++) {
+        if (text.indexOf(keywords[i]) !== -1) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function copyResult() {
@@ -40,13 +58,21 @@ function copyResult() {
     alert("Texto copiado al portapapeles.");
 }
 
+/*
+const internalText = document.getElementById("internal-text");
+internalText.value = "Ningún mensaje fue encontrado";
+
+
 const externalText = document.getElementById("external-text");
 const internalText = document.getElementById("internal-text");
 
 externalText.addEventListener("input", function() {
     if (externalText.value.trim() !== "") {
-        internalText.value = "Ningun mensaje fue encontrado " + externalText.value;
-    } else {
         internalText.value = "";
+    } else {
+        internalText.value = "Ningún mensaje fue encontrado";
     }
+    
 });
+*/
+
